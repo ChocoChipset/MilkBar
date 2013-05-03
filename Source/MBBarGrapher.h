6e8fa5d6fd9e734d7d35640eea7fd9b60e97b766
Hector Zarate <hecktorzr@gmail.com>
2013-05-03 15:09:06 +0200

    Created bar grapher class

---
+++ Source/MBBarGrapher.h
@@ -0,0 +1,13 @@
+//
+//  MBBarGrapher.h
+//  Example
+//
+//  Created by Hector Zarate on 5/3/13.
+//  Copyright (c) 2013 Hector Zarate. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MBBarGrapher : NSObject
+
+@end
---
+++ Source/MBBarGrapher.m
@@ -0,0 +1,13 @@
+//
+//  MBBarGrapher.m
+//  Example
+//
+//  Created by Hector Zarate on 5/3/13.
+//  Copyright (c) 2013 Hector Zarate. All rights reserved.
+//
+
+#import "MBBarGrapher.h"
+
+@implementation MBBarGrapher
+
+@end
