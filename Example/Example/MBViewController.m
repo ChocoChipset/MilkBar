8699ceb42bdfcb44ac74ef7df3c1655d141688be
Hector Zarate <hecktorzr@gmail.com>
2013-05-06 14:46:23 +0200

    Updated example to display extra fanciness (more values and different outline color

--- Example/Example/MBViewController.m
+++ Example/Example/MBViewController.m
@@ -19,14 +19,15 @@
 {
     [super viewDidLoad];
     
-    NSArray *exampleValues = @[@23, @35.5, @20, @70.2, @45.5, @47.7, @13.2, @33.3, @69.999, @83.25, @90.2, @99.9];
+    NSArray *exampleValues = @[@23, @35.5, @20, @70.2, @45.5, @47.7, @13.2, @33.3, @69.999, @83.25, @90.2, @99.9, @120.0];
     
     MBBarGrapher *barGrapher = [[MBBarGrapher alloc] initWithValues:exampleValues];
     
     CGFloat orangeColorComponents[4]    = { 1.0, 0.77, 0.29, 1.0 };
-    CGFloat blackColorComponents[4]     = { 0.0, 0.0, 0.0, 1.0 };
+    CGFloat blackColorComponents[4]     = { 0.0, 0.0, 0.0, 0.7 };
 
-
+    barGrapher.percentageOfSpaceBetweenBars = 0.2;
+    
     barGrapher.fillColor = [UIColor colorWithRed:orangeColorComponents[0]
                                            green:orangeColorComponents[1]
                                             blue:orangeColorComponents[2]
@@ -39,6 +40,7 @@
     
 
     self.graph.image = [barGrapher generateImageForSize:self.graph.frame.size];
+    
 }
 
 - (void)didReceiveMemoryWarning
